e14fb588c082de06eb9f33f292be86dca9921901
Антонов Андрей <an_42@mail.ru>
2021-05-17 13:17:41 +0700

    Initial Commit

---
+++ iOSCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCourse
+//
+//  Created by Андрей Антонов on 17/05/2021.
+//  Copyright © 2021 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
